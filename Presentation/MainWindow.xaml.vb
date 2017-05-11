d46540632b5758add4de744077dab9c217a3c509
trumpsilver <vyphamhung10@gmail.com>
2017-05-12 00:13:40 +0700

    èw

--- Presentation/MainWindow.xaml.vb
+++ Presentation/MainWindow.xaml.vb
@@ -21,25 +21,24 @@ Class MainWindow
     Public Sub SetupHarburgerBar()
         Dim mainMenuItems As New List(Of Domain.GroupMenuItem)
 
-        Dim menuItems1 As New Domain.GroupMenuItem With {.Name = "PHIẾU KHÁM BỆNH"}
+        Dim menuItems1 As New Domain.GroupMenuItem With {.Name = "KHÁM BỆNH"}
         menuItems1.MenuItems.Add(New Domain.MenuItem() With {.Name = "Lập phiếu khám bệnh", .Content = New ucPhieuKhamBenh()})
+        menuItems1.MenuItems.Add(New Domain.MenuItem() With {.Name = "Lập chi tiết phiếu khám", .Content = New ucPhieuKham()})
+        menuItems1.MenuItems.Add(New Domain.MenuItem() With {.Name = "Lập hóa đơn", .Content = New ucHoaDonThanhToan()})
         mainMenuItems.Add(menuItems1)
 
-        Dim menuItems2 As New Domain.GroupMenuItem With {.Name = "CHI TIẾT PHIẾU KHÁM"}
-        menuItems2.MenuItems.Add(New Domain.MenuItem() With {.Name = "Lập chi tiết phiếu khám", .Content = New ucPhieuKham()})
+
+        Dim menuItems2 As New Domain.GroupMenuItem With {.Name = "TRA CỨU"}
+        menuItems2.MenuItems.Add(New Domain.MenuItem() With {.Name = "Tra cứu bệnh nhân", .Content = New ucTraCuuBenhNhan()})
+        menuItems2.MenuItems.Add(New Domain.MenuItem() With {.Name = "Tra cứu bệnh nhân V2", .Content = New ucTraCuuBenhNhanV2()})
         mainMenuItems.Add(menuItems2)
 
-        Dim menuItems3 As New Domain.GroupMenuItem With {.Name = "TRA CỨU"}
-        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Tra cứu bệnh nhân", .Content = New ucTraCuuBenhNhan()})
-        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Tra cứu bệnh nhân V2", .Content = New ucTraCuuBenhNhanV2()})
+        Dim menuItems3 As New Domain.GroupMenuItem With {.Name = "DANH MỤC"}
+        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại bệnh", .Content = New ucLoaiBenh()})
+        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại thuốc", .Content = New ucLoaiThuoc()})
+        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại đơn vị", .Content = New ucLoaiDonVi()})
+        menuItems3.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại cách dùng", .Content = New ucLoaiCachDung()})
         mainMenuItems.Add(menuItems3)
-
-        Dim menuItems4 As New Domain.GroupMenuItem With {.Name = "DANH MỤC"}
-        menuItems4.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại bệnh", .Content = New ucLoaiBenh()})
-        menuItems4.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại thuốc", .Content = New ucLoaiThuoc()})
-        menuItems4.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại đơn vị", .Content = New ucLoaiDonVi()})
-        menuItems4.MenuItems.Add(New Domain.MenuItem() With {.Name = "Loại cách dùng", .Content = New ucLoaiCachDung()})
-        mainMenuItems.Add(menuItems4)
         trvMenu.ItemsSource = mainMenuItems
     End Sub
 End Class
---
+++ Presentation/ucHoaDonThanhToan.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class ucHoaDonThanhToan
+
+End Class
