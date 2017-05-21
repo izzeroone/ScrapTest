37433188f602db77f77a44e8d83afe5476530973
trumpsilver <vyphamhung10@gmail.com>
2017-05-21 10:44:16 +0700

    GUI 90%

--- Presentation/ucBaoCaoDoanhThu.xaml.vb
+++ Presentation/ucBaoCaoDoanhThu.xaml.vb
@@ -6,8 +6,12 @@ Public Class ucBaoCaoDoanhThu
     Private Sub tbNgayKham_SelectedDateChanged(sender As Object, e As SelectionChangedEventArgs)
         Dim tong As Long
         list = BaoCaoDoanhThuBUS.GetBaoCaoDoanhThu(tbNgayKham.SelectedDate, tong)
-        If (dgChiTietDoanhThu IsNot Nothing) Then
-            dgChiTietDoanhThu.ItemsSource = list
+        If dgChiTietDoanhThu IsNot Nothing Then
+            If (list.Count <> 0) Then
+                dgChiTietDoanhThu.ItemsSource = list
+            Else
+                dgChiTietDoanhThu.ItemsSource = New ObservableCollection(Of BaoCaoDoanhThuDTO)
+            End If
         End If
     End Sub
 End Class
